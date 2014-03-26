638836821265d512080c978eee435b54793b75d1
Giorgix <jackosinapsis@gmail.com>
2014-03-26 13:11:54 -0400

    deleted zoho verify html

--- app.py
+++ app.py
@@ -101,9 +101,3 @@ app.add_url_rule(
         'about', template_name='about/about.html')
 )
 app.add_url_rule('/contact/', view_func=Contact.as_view('contact'))
-
-app.add_url_rule(
-    '/zohoverify/verifyforzoho.html',
-    view_func=RenderTemplateView.as_view(
-        'zohoverify', template_name='zohoverify/verifyforzoho.html')
-)
